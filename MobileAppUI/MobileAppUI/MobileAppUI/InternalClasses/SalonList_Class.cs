2cc50de5fc2e1393dc55cc7daf0d0295e2ec7897
Dhruv Kanojia <kanojia24.10@gmail.com>
2018-09-11 17:00:29 +0530

    Salon List and Search Implemented
    List of salons and search implemented.

---
+++ MobileAppUI/MobileAppUI/MobileAppUI/InternalClasses/SalonList_Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileAppUI.InternalClasses
+{
+    class SalonList_Class
+    {
+        public int sId { get; set; }
+        public string sName { get; set; }
+        public string sState { get; set; }
+        public string sAccountNumber { get; set; }
+        public string sStatus { get; set; }
+
+    }
+}
